93840b080f0b918b0423d10877944a076bad01f0
Mariana Tamay <maritamay4@gmail.com>
2023-03-13 19:44:38 -0300

    API20231-35 ✨ Feat: Criação da página de listagem de alertas

--- api/src/routes/routes.tsx
+++ api/src/routes/routes.tsx
@@ -3,13 +3,15 @@ import { Route, BrowserRouter, Routes as Switch } from "react-router-dom";
 // pages ✨
 import Alertas from "../pages/criar-alertas";
 import Usuarios from "../pages/usuarios";
+import Alerts from "../pages/alertas";
 
 const Routes = () => {
     return (
         <BrowserRouter>
             <Switch>
-                <Route path="/alertas" element={<Alertas />} />
+                <Route path="/create-alertas" element={<Alertas />} />
                 <Route path="/usuarios" element={<Usuarios />} />
+                <Route path="/alertas" element={<Alerts />} />
             </Switch>
         </BrowserRouter>
     )
