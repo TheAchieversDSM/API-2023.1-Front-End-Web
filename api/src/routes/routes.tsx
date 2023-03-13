93e284aa44281ab5d3370d838091c99fd7d0adf2
Mariana Tamay <maritamay4@gmail.com>
2023-03-13 19:29:37 -0300

    API20231-33 ✨ Feat: Criação da página de listagem de estações

--- api/src/routes/routes.tsx
+++ api/src/routes/routes.tsx
@@ -3,6 +3,7 @@ import { Route, BrowserRouter, Routes as Switch } from "react-router-dom";
 // pages ✨
 import Alertas from "../pages/criar-alertas";
 import Usuarios from "../pages/usuarios";
+import Estacao from "../pages/estacao";
 
 const Routes = () => {
     return (
@@ -10,6 +11,7 @@ const Routes = () => {
             <Switch>
                 <Route path="/alertas" element={<Alertas />} />
                 <Route path="/usuarios" element={<Usuarios />} />
+                <Route path="/estacoes" element={<Estacao />} />
             </Switch>
         </BrowserRouter>
     )
