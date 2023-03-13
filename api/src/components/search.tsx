764d5dc1479897f6ce1faea9621c893d015a4482
Mariana Tamay <maritamay4@gmail.com>
2023-03-13 12:15:56 -0300

    API20231-36 📋 Tarefa: componente search

---
+++ api/src/components/search.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../styles/search.css';
+import Button from 'react-bootstrap/Button';
+
+export default function Search(){
+    return(
+        <div className="box-search">
+            <input className="input-search"></input>
+            <Button variant="primary" className="button-new">+ Novo</Button>
+        </div>
+    )
+}
