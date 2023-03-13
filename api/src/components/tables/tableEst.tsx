0904712ee2e9780760786d191d7648f1363ea671
Mariana Tamay <maritamay4@gmail.com>
2023-03-13 19:30:09 -0300

    API20231-63 📋 Tarefa: Criação da tabela de estações

---
+++ api/src/components/tables/tableEst.tsx
@@ -0,0 +1,30 @@
+import Table from 'react-bootstrap/Table';
+import "../../styles/table.css"
+import BG from '../buttonsGroup';
+
+export default function TableEst() {
+  return (
+    <div className="box-list">
+        <Table className="table" striped bordered hover size="sm" >
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Nome</th>
+                    <th>Latitude</th>
+                    <th>Longitude</th>
+                    <th></th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>6583</td>
+                    <td>Station</td>
+                    <td>-28.1845</td>
+                    <td>32.9533</td>
+                    <td><BG/></td>
+                </tr>
+            </tbody>
+        </Table>
+    </div>
+  )
+}
