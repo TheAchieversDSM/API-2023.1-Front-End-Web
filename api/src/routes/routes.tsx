35a15afd1bfcc379bf23f8263828d15d1193683c
Mariana Tamay <maritamay4@gmail.com>
2023-03-14 19:39:05 -0300

    API20231-62 ✨ Feat: Criação da página HOME

--- api/src/routes/routes.tsx
+++ api/src/routes/routes.tsx
@@ -2,12 +2,14 @@ import { Route, BrowserRouter, Routes as Switch } from "react-router-dom";
 
 // pages ✨
 import Alertas from "../pages/alertas";
+import HOME from "../pages/home";
 
 const Routes = () => {
     return (
         <BrowserRouter>
             <Switch>
                 <Route path="/alertas" element={<Alertas />} />
+                <Route path="/" element={<HOME />} />
             </Switch>
         </BrowserRouter>
     )
