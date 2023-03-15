aef4d5bc2459c4e6e1c15a316bb7950ab09eb555
MatheusSakuragui <matheus.lemesakuragui@gmail.com>
2023-03-15 10:16:17 -0300

    API20231-65 🐛 fix: Corrigido a extensao dos arquivos do grafico

--- api/src/utils/chart_utils/chartMount/chartMount.js
+++
@@ -1,9 +0,0 @@
-import Options from "../options/options";
-
-export default function chartMount(metrics){
-    var opts = Options
-    metrics.forEach(metrica => {
-        opts.series.push(metrica)
-    }); 
-    return opts
-}
