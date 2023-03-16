bb881516f1260b26b1a24844d1b0960ffe46641b
MatheusSakuragui <matheus.lemesakuragui@gmail.com>
2023-03-16 20:30:36 -0300

    API20231-58 ✨ feat: Criado tela do dashboard com gráfico.

--- api/src/pages/dashboard.tsx
+++ api/src/pages/dashboard.tsx
@@ -3,27 +3,54 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Sidebar from '../components/sidebar';
+import ButtonMain from '../components/button';
 
 import '../styles/dashboard.css'
 import Chart from '../components/chart';
 
 export default function Dashboard() {
+    const options = {
+        chart: {
+            type: 'spline'
+        },
+        title: {
+            text: 'Temperaturas diárias'
+        },
+        xAxis: {
+            categories: ['Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado', 'Domingo']
+        },
+        yAxis: {
+            title: {
+                text: 'Temperatura (°C)'
+            }
+        },
+        series: [{
+            name: 'Máxima',
+            data: [26, 25, 23, 20, 18, 20, 22]
+        }, {
+            name: 'Mínima',
+            data: [18, 17, 16, 13, 11, 13, 15]
+        }],
+        tooltip: {
+            shared: true,
+            valueSuffix: '°C'
+        }
+    }
     return (
         <>
             <Sidebar />
             <div className='main-body'>
                 <h1>Estação Fatec-SJC</h1>
-                <Container>
-                    <Row>
-                        <Col className=''>
-                                
-                        </Col>
-                        <Col className=''>
-                        </Col>
-                    </Row>
+                <div className='buttons'>
+                    <ButtonMain label="Todos" />
+                    <ButtonMain label="Param-A" />
+                    <ButtonMain label="Param-B" />
+                </div>
+                <Container className='boxes'>
+                    <Chart options={options} />
                 </Container>
             </div>
-            
+
         </>
     )
 }
