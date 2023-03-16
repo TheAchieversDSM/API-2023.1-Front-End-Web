6994cb1513f187c786aeffaeaf4fcbc6ec6f53af
Maroa Clara Alves de Faria <mclaralvs@gmail.com>
2023-03-16 20:56:28 -0300

    API20231-30 🐛 fix: Acrescentando a nova rota para criação de usuários.

--- api/src/routes/routes.tsx
+++ api/src/routes/routes.tsx
@@ -2,12 +2,14 @@ import { Route, BrowserRouter, Routes as Switch } from "react-router-dom";
 
 // pages ✨
 import Alertas from "../pages/criar-alertas";
+import Usuarios from "../pages/criar-usuarios";
 
 const Routes = () => {
     return (
         <BrowserRouter>
             <Switch>
                 <Route path="/create-alertas" element={<Alertas />} />
+                <Route path="/create-usuarios" element={<Usuarios />} />
             </Switch>
         </BrowserRouter>
     )
