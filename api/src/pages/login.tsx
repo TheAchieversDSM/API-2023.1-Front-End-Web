8d21d34a8e9cc894196ff61e8202be2527547e37
mclaralvs <mclaralvs@gmail.com>
2023-03-17 22:05:20 -0300

    🐛 fix: Ajustando o CSS do login e adicionando padding na barra de Search.

--- api/src/pages/login.tsx
+++ api/src/pages/login.tsx
@@ -10,30 +10,32 @@ import '../styles/login.css'
 export default function Login() {
     return (
         <div className="body-login">
-            <img src={logo} alt="logo" />
+            <div className="grid-login">
+                <img src={logo} alt="logo" />
 
-            <div className="box-glass">
-                <Row className='login-fields'>
-                    <Col md={10}>
-                        <Input
-                            label="E-mail"
-                            size="mb-6"
-                            type="email"
-                            placeholder="Insira seu e-mail."
-                        />
+                <div className="box-glass">
+                    <Row className='login-fields'>
+                        <Col md={10}>
+                            <Input
+                                label="E-mail"
+                                size="mb-6"
+                                type="email"
+                                placeholder="Insira seu e-mail."
+                            />
 
-                        <Input
-                            label="Senha"
-                            size="mb-6"
-                            type="passoword"
-                            placeholder="Insira sua senha."
-                        />
+                            <Input
+                                label="Senha"
+                                size="mb-6"
+                                type="passoword"
+                                placeholder="Insira sua senha."
+                            />
 
-                        <div className="login-button">
-                            <Button label="Log In"/>
-                        </div>
-                    </Col>
-                </Row>
+                            <div className="login-button">
+                                <Button label="Log In"/>
+                            </div>
+                        </Col>
+                    </Row>
+                </div>
             </div>
         </div>
     )
