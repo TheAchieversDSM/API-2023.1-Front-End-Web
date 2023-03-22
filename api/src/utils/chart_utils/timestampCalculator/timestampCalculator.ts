beae78eeb1b92bae26e4124c7d24841b15077ce9
MatheusSakuragui <matheus.lemesakuragui@gmail.com>
2023-03-22 10:45:01 -0300

    API20231-101 🐛 fix: Alterado timestampCalculator.ts

--- api/src/utils/chart_utils/timestampCalculator/timestampCalculator.ts
+++ api/src/utils/chart_utils/timestampCalculator/timestampCalculator.ts
@@ -4,8 +4,22 @@ import 'moment-range'
 
 
 
-export default function timestampCalculator(start_date: Date, end_date: Date){
+export default function timestampCalculator(start_date: Date, end_date: Date) {
     const moment = extendMoment(Moment);
-    const range = moment.range(start_date, end_date)
-    
+    const range = moment.range(moment(start_date), moment(end_date))
+
+    const datas = Array.from(range.by('day'));
+
+    var timestamps = []
+
+    for (let i = 0; i < datas.length; i++) {
+        const date = datas[i];
+
+        const timestamp = moment(date).valueOf();
+
+        timestamps.push(timestamp)
+    }
+
+    return timestamps
+
 }
