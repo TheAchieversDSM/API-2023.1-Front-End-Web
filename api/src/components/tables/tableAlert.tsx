3cbbd015e57477a7ca918fc5f17beb9ea5bc8e6d
Mariana Tamay <maritamay4@gmail.com>
2023-03-28 22:56:07 -0300

    API20231-100 📋 Tarefa: Conclusão dos input search

--- api/src/components/tables/tableAlert.tsx
+++ api/src/components/tables/tableAlert.tsx
@@ -5,6 +5,7 @@ import MyVerticallyCenteredModal from '../modal';
 import { BsTrash3, BsEye, BsPencil, BsClipboard2 } from 'react-icons/bs'
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import Search from '../search';
 
 let modelo = [
     {
@@ -20,6 +21,7 @@ let modelo = [
 export default function TableAlert() {
     const [alertas, setAlertas] = useState(modelo)
     const [modalShow, setModalShow] = React.useState(false);
+    const [searchTerm, setSearchTerm] = useState('');
 
     useEffect(() =>{
         function render(){
@@ -30,47 +32,71 @@ export default function TableAlert() {
         render()
     },[])
 
+    function handleSearch(event: React.ChangeEvent<HTMLInputElement>) {
+        setSearchTerm(event.target.value);
+    }
+
+    function renderTableRows() {
+        return alertas
+          .filter((alerta) => {
+            if (!searchTerm) {
+              return true;
+            }
+    
+            if (
+              alerta.id.toString().toLowerCase().includes(searchTerm.toLowerCase())
+            ) {
+              return true;
+            }
+    
+            return false;
+          })
+          .map((alerta) => (
+            <tr>
+                <td>{alerta.id}</td>
+                <td>{alerta.id_estacao}</td>
+                <td>{alerta.id_parametro}</td>
+                <td>{alerta.valorMax}</td>
+                <td>{alerta.valorMinimo}</td>
+                <td>
+                    <Button className="bt bt-record"><BsClipboard2 className="icon" /></Button>
+                    <Button className="bt bt-view"><BsEye className="icon" onClick={() => setModalShow(true)} /></Button>
+                    <Button className="bt bt-edit"><BsPencil className="icon" /></Button>
+                    <Button className="bt bt-delete"><BsTrash3 className="icon" /></Button>
+
+                    <MyVerticallyCenteredModal
+                        show={modalShow}
+                        onHide={() => setModalShow(false)}
+                        titulo={alerta.id}
+                        coluna1="ID: " resp1={alerta.id}
+                        coluna2="Estação: " resp2={alerta.id_estacao}
+                        coluna3="Parâmetro: " resp3={alerta.id_parametro}
+                        coluna4="Valor Máximo: " resp4={alerta.valorMax}
+                        coluna5="Valor Minimo: " resp5={alerta.valorMinimo} />
+                </td>
+            </tr>
+          ));
+      }
+
   return (
+    <>
+    <Search change={handleSearch} link="/criar-alertas"/>
     <div className="box-list">
-        <Table className="table" size="sm" >
-            <thead>
-                <tr>
-                    <th>ID</th>
-                    <th>Estação</th>
-                    <th>Parâmetro</th>
-                    <th>Valor Máximo</th>
-                    <th>Valor Minimo</th>
-                    <th></th>
-                </tr>
-            </thead>
-            <tbody>
-                {alertas.map(alerta =>
-                    <tr>
-                    <td>{alerta.id}</td>
-                    <td>{alerta.id_estacao}</td>
-                    <td>{alerta.id_parametro}</td>
-                    <td>{alerta.valorMax}</td>
-                    <td>{alerta.valorMinimo}</td>
-                    <td>      
-                        <Button className="bt bt-record"><BsClipboard2 className="icon"/></Button>      
-                        <Button className="bt bt-view"><BsEye className="icon" onClick={() => setModalShow(true)}/></Button>
-                        <Button className="bt bt-edit"><BsPencil className="icon"/></Button>
-                        <Button className="bt bt-delete"><BsTrash3 className="icon"/></Button>
-                        
-                        <MyVerticallyCenteredModal
-                            show={modalShow}
-                            onHide={() => setModalShow(false)}
-                            titulo={alerta.id}
-                            coluna1="ID: " resp1={alerta.id}
-                            coluna2="Estação: " resp2={alerta.id_estacao}
-                            coluna3="Parâmetro: " resp3={alerta.id_parametro}
-                            coluna4="Valor Máximo: " resp4={alerta.valorMax}
-                            coluna5="Valor Minimo: " resp5={alerta.valorMinimo}
-                        />
-                    </td>
-                </tr>)}
-            </tbody>
-        </Table>
-    </div>
+          <Table className="table" size="sm">
+              <thead>
+                  <tr>
+                      <th>ID</th>
+                      <th>Estação</th>
+                      <th>Parâmetro</th>
+                      <th>Valor Máximo</th>
+                      <th>Valor Minimo</th>
+                      <th></th>
+                  </tr>
+              </thead>
+              <tbody>
+                {renderTableRows()} 
+              </tbody>
+          </Table>
+      </div></>
   )
 }
--- api/src/components/tables/tablePar.tsx
+++ api/src/components/tables/tablePar.tsx
@@ -5,6 +5,7 @@ import MyVerticallyCenteredModal from '../modal';
 import { BsTrash3, BsEye, BsPencil } from 'react-icons/bs'
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import Search from '../search';
 
 let modelo = [
     {
@@ -20,6 +21,7 @@ let modelo = [
 export default function TablePar(props:any) {
     const [parametros, setParametros] = useState(modelo)
     const [modalShow, setModalShow] = React.useState(false);
+    const [searchTerm, setSearchTerm] = useState('');
 
     useEffect(() => {
         function render(){
@@ -30,44 +32,68 @@ export default function TablePar(props:any) {
         render()
     }, [])
 
+    function handleSearch(event: React.ChangeEvent<HTMLInputElement>) {
+        setSearchTerm(event.target.value);
+    }
+
+    function renderTableRows() {
+        return parametros
+          .filter((parametro) => {
+            if (!searchTerm) {
+              return true;
+            }
+    
+            if (
+              parametro.nome.toLowerCase().includes(searchTerm.toLowerCase()) ||
+              parametro.tipo.toLowerCase().includes(searchTerm.toLowerCase()) ||
+              parametro.id.toString().toLowerCase().includes(searchTerm.toLowerCase())
+            ) {
+              return true;
+            }
+    
+            return false;
+          })
+          .map((parametro) => (
+            <tr key={parametro.id}>
+              <td>{parametro.id}</td>
+              <td>{parametro.nome}</td>
+              <td>{parametro.tipo}</td>
+              <td>
+                <Button className="bt bt-view"><BsEye className="icon" onClick={() => setModalShow(true)} /></Button>
+                <Button className="bt bt-edit"><BsPencil className="icon" /></Button>
+                <Button className="bt bt-delete"><BsTrash3 className="icon" /></Button>
+                <MyVerticallyCenteredModal
+                    show={modalShow}
+                    onHide={() => setModalShow(false)}
+                    titulo={parametro.nome}
+                    coluna1="ID: " resp1={parametro.id}
+                    coluna3="Tipo: " resp3={parametro.tipo}
+                    coluna4="Unidade de medida: " resp4={parametro.unidadeDeMedida}
+                    coluna5="Fator: " resp5={parametro.fator}
+                    coluna6="OffSet: " resp6={parametro.offset} />
+              </td>
+            </tr>
+          ));
+      }
+
   return (
+    <>
+    <Search change={handleSearch} link="/criar-parametros" />
     <div className="box-list">
-        <Table className="table" size="sm" >
-            <thead>
-                <tr>
-                    <th>ID</th>
-                    <th>Nome</th>
-                    <th>Tipo</th>
-                    <th>Unidade de medida</th>
-                    <th></th>
-                </tr>
-            </thead>
-            <tbody>
-                {parametros.map(parametro =>
-                    <tr>
-                        <td>{parametro.id}</td>
-                        <td>{parametro.nome}</td>
-                        <td>{parametro.tipo}</td>
-                        <td>{parametro.unidadeDeMedida}</td>
-                        <td>            
-                            <Button className="bt bt-view"><BsEye className="icon" onClick={() => setModalShow(true)}/></Button>
-                            <Button className="bt bt-edit"><BsPencil className="icon"/></Button>
-                            <Button className="bt bt-delete"><BsTrash3 className="icon"/></Button>
-                            <MyVerticallyCenteredModal
-                                show={modalShow}
-                                onHide={() => setModalShow(false)}
-                                titulo={parametro.nome}
-                                coluna1="ID: " resp1={parametro.id}
-                                coluna3="Tipo: " resp3={parametro.tipo}
-                                coluna4="Unidade de medida: " resp4={parametro.unidadeDeMedida}
-                                coluna5="Fator: " resp5={parametro.fator}
-                                coluna6="OffSet: " resp6={parametro.offset}
-                            />
-                        </td>
-                    </tr>
-                )}
-            </tbody>
-        </Table>
-    </div>
+          <Table className="table" size="sm">
+              <thead>
+                  <tr>
+                      <th>ID</th>
+                      <th>Nome</th>
+                      <th>Tipo</th>
+                      <th>Unidade de medida</th>
+                      <th></th>
+                  </tr>
+              </thead>
+              <tbody>
+                {renderTableRows()}
+              </tbody>
+          </Table>
+      </div></>
   )
 }
