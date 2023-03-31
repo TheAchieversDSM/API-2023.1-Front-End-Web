39692f3582770a1a5c4e2ff8569460c09316bbd5
Maroa Clara Alves de Faria <mclaralvs@gmail.com>
2023-03-31 10:39:13 -0300

    API20231-118 🐛 fix: Readicionando o alerta de cadastro.

--- api/src/pages/criar-alertas.tsx
+++ api/src/pages/criar-alertas.tsx
@@ -66,6 +66,8 @@ export default function CriarAlertas() {
                 nivel: alerta.nivel,
             })
             .then((res) => { });
+
+        alert("Alerta cadastrada!")
     };
 
     return (
