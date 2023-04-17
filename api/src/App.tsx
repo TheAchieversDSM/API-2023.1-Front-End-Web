9b17629d7c6e9f45d9e4402f04f9b75bceaf9755
ZeroPirata <gabriel0gsbn@gmail.com>
2023-04-16 23:06:59 -0300

    API20231-178-Auth-Front 🐛 Fix: Correção da renderização do componente de Routes

--- api/src/App.tsx
+++ api/src/App.tsx
@@ -1,15 +1,19 @@
-import React from 'react';
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
 // styles ✨
-import './styles/global.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "./styles/global.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 // components ✨
-import Routes from './routes/routes';
+import Routes from "./routes/routes";
+import { AuthProvider } from "./hooks/useAuth";
 
 export default function App() {
-    return (
-        <>
-            <Routes />
-        </>
-    );
+  return (
+    <BrowserRouter>
+      <AuthProvider>
+        <Routes />
+      </AuthProvider>
+    </BrowserRouter>
+  );
 }
