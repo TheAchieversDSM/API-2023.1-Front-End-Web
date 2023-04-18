da0d6d1e9740c0ac4f09f477d24e68bdd75e3a32
MatheusSakuragui <matheus.lemesakuragui@gmail.com>
2023-04-18 11:35:19 -0300

    API20231-143 📋 task: Adicionado accordions para exibir parametros no dashboard

--- api/src/pages/dashboard.tsx
+++ api/src/pages/dashboard.tsx
@@ -1,91 +1,107 @@
-import axios from 'axios';
+import axios from "axios";
 import { useParams } from "react-router-dom";
-import Sidebar from '../components/sidebar';
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 
-import '../styles/dashboard.css'
-import Chart from '../components/chart';
-import { EstacaoParametro, MediasSeries } from '../utils/types/types';
-import Navigation from '../components/nav/nav';
-import NavItem from '../components/nav/navItem';
-import metricMount from '../utils/chart_utils/metricMount/metricMount';
-import Options from '../utils/chart_utils/options/options';
-import averageCalculator from '../utils/chart_utils/averageCalculator/averageCalculator';
-import chartMount from '../utils/chart_utils/chartMount/chartMount';
-import groupByUnixtime from '../utils/chart_utils/groupUnixtime/groupUnixtime';
+import "../styles/dashboard.css";
+import Chart from "../components/chart";
+import Sidebar from "../components/sidebar";
+import Navigation from "../components/nav/nav";
+import NavItem from "../components/nav/navItem";
+import Accordion from "react-bootstrap/Accordion";
+
+import { EstacaoParametro, MediasSeries } from "../utils/types/types";
+import metricMount from "../utils/chart_utils/metricMount/metricMount";
+import Options from "../utils/chart_utils/options/options";
+import averageCalculator from "../utils/chart_utils/averageCalculator/averageCalculator";
+import chartMount from "../utils/chart_utils/chartMount/chartMount";
+import groupByUnixtime from "../utils/chart_utils/groupUnixtime/groupUnixtime";
 
 export default function Dashboard() {
-    const { id } = useParams()
-    const [estacaoNome, setEstacaoNome] = useState()
-    const [estacaoParametros, setEstacaoParametros] = useState<[EstacaoParametro]>();
-    const [paramId, setParamId ] = useState(0)
-    const [parametroDisplay, setParametroDisplay] = useState<EstacaoParametro>();
-    const [medidas, setMedidas] = useState<Array<MediasSeries>>();
-    const [options, setOptions] = useState<Options>()
-    useEffect(() => {
-        function render() {
-            axios.get(`http://localhost:5000/parametro/pegarMedidaEstacaoParametro/${id}`).then(res => {
-                setEstacaoNome(res.data.nome)
-                setEstacaoParametros(res.data)
-            })
+  const { id } = useParams();
+  const [estacaoNome, setEstacaoNome] = useState();
+  const [estacaoParametros, setEstacaoParametros] =
+    useState<[EstacaoParametro]>();
+  const [paramId, setParamId] = useState(0);
+  const [parametroDisplay, setParametroDisplay] = useState<EstacaoParametro>();
+  const [medidas, setMedidas] = useState<Array<MediasSeries>>();
+  const [options, setOptions] = useState<Options>();
+  useEffect(() => {
+    function render() {
+      axios
+        .get(
+          `http://localhost:5000/parametro/pegarMedidaEstacaoParametro/${id}`
+        )
+        .then((res) => {
+          setEstacaoNome(res.data.nome);
+          setEstacaoParametros(res.data);
+        });
+    }
 
+    render();
+  }, []);
+
+  useEffect(() => {
+    const medidasseries: MediasSeries[] = [];
+    estacaoParametros?.map((parametro: EstacaoParametro) => {
+      if (parametro.medidas[0].unixtime) {
+        parametro.medidaMedia = averageCalculator(
+          groupByUnixtime(parametro.medidas)
+        );
+        const med = {
+          nome: parametro.nome,
+          sufixo: {
+            nome: " " + parametro.unidadeDeMedida.nome,
+            id: parametro.unidadeDeMedida.unidade_id,
+          },
+          media: parametro.medidaMedia,
+        };
+        medidasseries.push(med);
+      }
+    });
+    setMedidas(medidasseries);
+
+    if (medidas) {
+      const metrics = metricMount(medidas);
+      setOptions(chartMount(metrics));
+    }
+  }, [estacaoParametros]);
+
+  useEffect(() => {
+    if (paramId != 0) {
+      estacaoParametros?.map((estacao) => {
+        if (estacao.parametro_id == paramId) {
+          setParametroDisplay(estacao);
+          console.log(parametroDisplay);
         }
+      });
+    }
+  }, [paramId]);
 
-        render()
-    }, [])
-
-    useEffect(() => {
-        const medidasseries: MediasSeries[] = []
-        estacaoParametros?.map((parametro: EstacaoParametro) => {
-            if (parametro.medidas[0].unixtime) {
-                parametro.medidaMedia = averageCalculator(groupByUnixtime(parametro.medidas))
-                const med = { nome: parametro.nome, sufixo: { nome: ' '+ parametro.unidadeDeMedida.nome, id: parametro.unidadeDeMedida.unidade_id }, media: parametro.medidaMedia }
-                medidasseries.push(med)
-            }
-        })
-        setMedidas(medidasseries)
-
-
-        if (medidas) {
-            const metrics = metricMount(medidas)
-            setOptions(chartMount(metrics))
-        }
-
-    }, [estacaoParametros])
-
-    useEffect(()=>{
-        if (paramId != 0){
-            estacaoParametros?.map(estacao=>{
-                if (estacao.parametro_id == paramId){
-                    setParametroDisplay(estacao)
-                    console.log(parametroDisplay)
-                }
-            })
-        }
-    }, [paramId])
-
-
-    return (
-        <>
-            <Sidebar />
-            <div className='main-body'>
-                <h1 className="TitImp">Estação {estacaoNome}</h1>
-                <div className='buttons_dashboard'>
-
-                    <Navigation variant="pills" default="1">
-                        <NavItem index={1} label="Todos" function={()=> {setParamId(0)}} />
-                        {estacaoParametros?.map((parametro, index) =>
-                            <NavItem index={index + 2} label={parametro.nome} function={()=> {setParamId(parametro.parametro_id)}}  />
-                        )}
-                    </Navigation>
-                </div>
-                <div className='container_dashboard'>
-                    <Chart className='container_dashboard' options={options} />
-                    
-
-                </div>
-            </div>
-
-        </>
-    )
+  return (
+    <>
+      <Sidebar />
+      <div className="main-body">
+        <h1 className="TitImp">Estação {estacaoNome}</h1>
+        <div className="container_dashboard">
+          <div className="box-dash">
+            <Chart className="container_dashboard" options={options} />
+          </div>
+          <h4 className="parametrosTitulo">Parâmetros</h4>
+          <div className="box-par">
+            <Accordion className="accordion">
+              {estacaoParametros?.map((param, index) => (
+                <Accordion.Item
+                  eventKey={index.toString()}
+                  className="accordion-item"
+                >
+                  <Accordion.Header>{param.nome}</Accordion.Header>
+                  <Accordion.Body>{param.formula}</Accordion.Body>
+                </Accordion.Item>
+              ))}
+            </Accordion>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
