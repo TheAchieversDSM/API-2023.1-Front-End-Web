cf9d8e642bdb8bea168a75947f2de10d463c19cd
Mariana Tamay <maritamay4@gmail.com>
2023-04-19 09:19:03 -0300

    API20231-174 📋 Tarefa: Correção da estilização

--- api/src/components/alerts.tsx
+++ api/src/components/alerts.tsx
@@ -8,27 +8,24 @@ export default function Alerts(){
   if (show) {
     return (
         <>
-            <Alert variant="danger" onClose={() => setShow(false)} dismissible className="alerta">
-                <Alert.Heading><b>CRÍTICO!</b></Alert.Heading>
-                <p>
-          
-                </p>
-            </Alert>
-
-            <Alert variant="warning" onClose={() => setShow(false)} dismissible className="alerta">
-                <Alert.Heading><b>PERIGO!</b></Alert.Heading>
-                <p>
-          
-                </p>
-            </Alert>
-
-            <Alert variant="secondary" onClose={() => setShow(false)} dismissible className="alerta">
-                <Alert.Heading><b>ATENÇÃO!</b></Alert.Heading>
-                <p>
-          
-                </p>
-            </Alert>
-        </>
+                <div className="alert-container">
+                    <Alert key="danger" variant="danger" onClose={() => setShow(false)} dismissible className="alerta">
+                        <Alert.Heading><b>CRÍTICO!</b></Alert.Heading>
+                        <p>
+                        </p>
+                    </Alert>
+                    <Alert key="warning" variant="warning" onClose={() => setShow(false)} dismissible className="alerta">
+                        <Alert.Heading><b>CRÍTICO!</b></Alert.Heading>
+                        <p>
+                        </p>
+                    </Alert>
+                <Alert key="dark" variant="dark" onClose={() => setShow(false)} dismissible className="alerta">
+                        <Alert.Heading><b>CRÍTICO!</b></Alert.Heading>
+                        <p>
+                        </p>
+                    </Alert>
+                </div>
+                </>
     );
   }
   return <Button onClick={() => setShow(true)} className="btn-alert">Show Alert</Button>;
