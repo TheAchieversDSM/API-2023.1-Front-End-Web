114a0bc4b93019107a642e6994ba0aff9ca1c7fd
MatheusSakuragui <matheus.lemesakuragui@gmail.com>
2023-04-21 19:01:46 -0300

    API20231-175-Funcao-identificar-alertas-tempo-real 📋 task: criado lógica para geração dos alertas

--- api/src/App.tsx
+++ api/src/App.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import axios from 'axios';
 
 // styles ✨
 import './styles/global.css';
@@ -11,6 +12,26 @@ import { AuthProvider } from "./hooks/useAuth";
 import { BrowserRouter } from 'react-router-dom';
 
 export default function App() {
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            axios.get(`http://localhost:5000/report/redis-alertas`).then((res) => {
+                res.data.map((report: any) => {
+                    if (report.value.nivel == "1") {
+
+                    }
+                    else if (report.value.nivel == "2") {
+
+                    } 
+                    else if (report.value.nivel == "3") {
+
+                    }
+                })
+            })
+
+        }, 60000);
+        return () => clearInterval(intervalId);
+    }, []);
+
     return (
         <BrowserRouter>
             <AuthProvider>
