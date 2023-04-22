474f102b844d0cf0fd92615b8df399e47972fb25
Maria Clara Alves de Faria <mclaralvs@gmail.com>
2023-04-22 18:53:31 -0300

    🌱 fix: Implementação do Tost para identificar alertas.

--- api/src/App.tsx
+++ api/src/App.tsx
@@ -9,64 +9,73 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Routes from './routes/routes';
 import Alerts from './components/alerts';
 import { AuthProvider } from "./hooks/useAuth";
-import { BrowserRouter } from 'react-router-dom';
-import AlertaAtencao from './components/alertas/alertaAtencao';
-import AlertaPerigo from './components/alertas/alertaPerigo';
-import AlertaCritico from './components/alertas/alertaCritico';
+import { BrowserRouter, Link } from 'react-router-dom';
 import Swal from 'sweetalert2';
 
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 export default function App() {
-    const Toast = Swal.mixin({
-        toast: true,
-        position: 'top-right',
-        iconColor: 'white',
-        customClass: {
-          popup: 'colored-toast'
-        },
-        showConfirmButton: true,
-        timer: 15000,
-        timerProgressBar: true
-      })
+    const [dados, setDados] = useState<any>([])
+
+    const attention = () => toast.info("Alerta: Atenção!", {
+        autoClose: 10000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "colored",
+    });
+
+    const perigo = () => toast.warn("Alerta: Perigo!", {
+        autoClose: 10000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "colored",
+    });
+
+    const critico = () => toast.error("Alerta: Crítico!", {
+        autoClose: 10000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "colored",
+    });
+    
     useEffect(() => {
         const intervalId = setInterval(() => {
             axios.get(`http://localhost:5000/report/redis-alertas`).then((res) => {
-                console.log(res.data)
+                setDados(res.data)
                 res.data.map((report: any) => {
                     if (report.value.nivel == "1") {
-                        console.log("aaa")
-                        Toast.fire({
-                            icon: 'info',
-                            title: 'Atenção'
-                          })
+                        attention()
                     }
-                    else if (report.value.nivel == "2") {
-                        console.log("aaa2")
-                        Toast.fire({
-                            icon: 'warning',
-                            title: 'Perigo'
-                          })
-                    } 
-                    else if (report.value.nivel == "3") {
-                        console.log("aaa3")
-                        Toast.fire({
-                            icon: 'error',
-                            title: 'Crítico'
-                          })
+                    else if (report.value.nivel == "2") {                        
+                        perigo()
+                    }
+                    else if (report.value.nivel == "3") {                        
+                        critico()
                     }
                 })
             })
 
         }, 60000);
-        return () =>{
+        return () => {
             clearInterval(intervalId)
         }
-        
+
     }, []);
 
     return (
         <BrowserRouter>
             <AuthProvider>
+                <Link to={`/reports/${dados[0].value.estacao}`}><ToastContainer /></Link>
                 <Routes />
             </AuthProvider>
         </BrowserRouter>
