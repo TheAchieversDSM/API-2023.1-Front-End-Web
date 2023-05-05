0861eb26cfb76e44bbce9135032e61318d8a44f9
Mariana Tamay <maritamay4@gmail.com>
2023-05-04 22:27:18 -0300

    API20231-191 📋 Tarefa: Alteração nas classes

--- api/src/pages/docAlerta.tsx
+++ api/src/pages/docAlerta.tsx
@@ -1,4 +1,3 @@
-import { ToastContainer } from "react-toastify";
 import Sidebar from "../components/sidebar";
 import "../styles/documentacao.css"
 
@@ -8,10 +7,10 @@ export default function DocAlerta(){
             <Sidebar />
             <h1 className="TitImp">Alertas</h1>
             <div className="box-doc">
-                <p className="doc-txt">Os alertas de Atenção, Perigo e Crítico são importantes classificações utilizadas para alertar a população sobre as condições meteorológicas adversas que podem ocorrer em determinada região</p>
-                <p className="doc-txt txt1"><b>Alerta de Atenção</b> é emitido quando há possibilidade de ocorrência de condições meteorológicas adversas, como chuvas intensas ou ventos fortes, que podem causar impactos pontuais.</p>
-                <p className="doc-txt txt2"><b>Alerta de Perigo</b> indica a possibilidade de ocorrência de fenômenos meteorológicos mais intensos e duradouros, como tempestades, inundações e deslizamentos de terra, que podem causar danos significativos à infraestrutura e à população.</p>
-                <p className="doc-txt txt3"><b>Alerta Crítico</b> é emitido quando as condições meteorológicas atingem níveis extremos e apresentam risco iminente à vida das pessoas, como furacões, ciclones e tornados.</p>
+                <p>Os alertas de Atenção, Perigo e Crítico são importantes classificações utilizadas para alertar a população sobre as condições meteorológicas adversas que podem ocorrer em determinada região</p>
+                <p className="txt1"><b>Alerta de Atenção</b> é emitido quando há possibilidade de ocorrência de condições meteorológicas adversas, como chuvas intensas ou ventos fortes, que podem causar impactos pontuais.</p>
+                <p className="txt2"><b>Alerta de Perigo</b> indica a possibilidade de ocorrência de fenômenos meteorológicos mais intensos e duradouros, como tempestades, inundações e deslizamentos de terra, que podem causar danos significativos à infraestrutura e à população.</p>
+                <p className="txt3"><b>Alerta Crítico</b> é emitido quando as condições meteorológicas atingem níveis extremos e apresentam risco iminente à vida das pessoas, como furacões, ciclones e tornados.</p>
             </div>
         </>
     )
