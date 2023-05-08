78d8d139d9e9e33b69eea2f6aaef775f176e765e
= <=>
2023-05-08 10:14:49 -0300

    API20231-156 📋 task: inserido range máximo padrão

--- api/src/utils/chart_utils/options/options.ts
+++ api/src/utils/chart_utils/options/options.ts
@@ -8,49 +8,20 @@ interface XAxis {
 
 export default class Options {
     chart = {
-        events: {
-            selection: function(event: any) {
-              console.log("Usuário fez uma seleção no gráfico:", event);
-              // aqui você pode chamar a função que desejar
-            }
-          },
         type: "spline",
         width: 900,
         height: 500,
         zoomType: 'x', // Adiciona a opção de zoom no eixo x
         panning: true, // Habilita o panning no eixo x
       };
-      xAxis = {categories: []}
+      xAxis = {categories: [], range: 5 }
       title = { text: `Informações meteorológicas da estação` };
-    
+      rangeSelector!: {
+        enabled: false
+    }
       series: Array<Metric>;
       lang = { noData: "Não há dados disponíveis para exibição." };
       noData = { style: { fontWeight: 'bold', fontSize: '24px', color: '#5751D3' } };
-      rangeSelector = {
-        inputEnabled: true,
-        buttons: [
-          {
-            type: 'day',
-            count: 1,
-            text: '1d'
-          },
-          {
-            type: 'day',
-            count: 7,
-            text: '1w'
-          },
-          {
-            type: 'month',
-            count: 1,
-            text: '1m'
-          },
-          {
-            type: 'all',
-            text: 'Tudo'
-          }
-        ],
-        selected: 0 // Define o intervalo inicial
-      };
 
     constructor(title: string, series: Array<Metric>) {
         this.title = { text: `Informações meteorológicas da estação ${title}` };
