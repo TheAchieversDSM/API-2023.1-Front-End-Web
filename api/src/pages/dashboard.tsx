124d24d259da7d6c3f942b4274cbce6766ccdaed
MatheusSakuragui <matheus.lemesakuragui@gmail.com>
2023-05-14 15:24:36 -0300

    Finalizado ajustes na separação do grafico e adicionado botao de reset

--- api/src/pages/dashboard.tsx
+++ api/src/pages/dashboard.tsx
@@ -6,9 +6,10 @@ import "../styles/dashboard.css";
 import Chart from "../components/chart";
 import Sidebar from "../components/sidebar";
 import Accordion from "react-bootstrap/Accordion";
-import { Row, Col } from 'react-bootstrap';
+import { Row, Col, Button } from 'react-bootstrap';
 import { EstacaoParametro, Medida } from "../utils/types/types";
 import Options from "../utils/chart_utils/options/options";
+import { MdUpdate } from "react-icons/md"
 import { parseCookies } from "nookies";
 import dadosDiarios from "../utils/chart_utils/dadosDiarios";
 import { generateOptions } from "../utils/chart_utils/options/optionsGenerate";
@@ -20,15 +21,13 @@ export default function Dashboard() {
 
   const estacaoNome = localStorage.getItem("estacaoNome") || ""
   const [estacaoParametros, setEstacaoParametros] = useState<EstacaoParametro[]>([]);
-  const [paramId, setParamId] = useState(0);
   const [renderButton, setRenderButton] = useState(false)
-  const [parametroDisplay, setParametroDisplay] = useState<EstacaoParametro>();
-  const [medidas, setMedidas] = useState<Medida[]>();
+  const [fetchData, setFetchData] = useState(true)
   const [options, setOptions] = useState<Options>();
   const [optionsState, setOptionsState] = useState<any>({});
 
   useEffect(() => {
-    function fetchDataFirstTime() {
+    function render() {
       axios
         .get(
           `http://localhost:5000/parametro/pegarMedidaEstacaoParametro/${id}`,
@@ -39,14 +38,12 @@ export default function Dashboard() {
 
         });
     }
-
-    fetchDataFirstTime();
-
-    return () => {
-      // Limpar qualquer recurso que precise ser limpo
-    };
-  }, []);
-
+    if (fetchData) {
+      render();
+      setFetchData(false)
+      setRenderButton(false)
+    }
+  }, [fetchData]);
 
   useEffect(() => {
     setOptions(generateOptions(estacaoParametros, estacaoNome))
@@ -56,22 +53,19 @@ export default function Dashboard() {
     const newEstacaoParametros = estacaoParametros.map((estacParam, index) => {
       const updatedEstacParam = { ...estacParam };
       updatedEstacParam.medidas = dadosDiarios(estacParam.medidas, category);
-      const opts = generateOptions([updatedEstacParam], estacaoNome ? estacaoNome : '');
+      const opts = generateOptions([updatedEstacParam], estacParam.nome);
       const newOpts = { ...optionsState, [index]: opts };
       setOptionsState(newOpts);
       return updatedEstacParam;
     });
-
-
+    setRenderButton(true)
     setEstacaoParametros(newEstacaoParametros);
   };
 
-
   useEffect(() => {
-
     const newOptionsState: any = {};
     estacaoParametros.forEach((param, index) => {
-      const opts = generateOptions([param], estacaoNome);
+      const opts = generateOptions([param], param.nome.bold() || estacaoNome);
       opts.setFuncao((event: any) => {
         setDadosDiarios(event.point.category)
       });
@@ -80,33 +74,23 @@ export default function Dashboard() {
     setOptionsState(newOptionsState);
   }, [estacaoParametros]);
 
-  useEffect(() => {
-    if (paramId != 0) {
-      estacaoParametros?.map((estacao) => {
-        if (estacao.parametro_id == paramId) {
-          setParametroDisplay(estacao);
-        }
-      });
-    }
-  }, [paramId]);
-
   return (
     <>
       <Sidebar />
       <div className="main-body">
         <h1 className="TitImp">{estacaoNome}</h1>
         <div className="container_dashboard">
+          {renderButton ? <Button className="chartResetButton" onClick={() => setFetchData(true)}><MdUpdate className="buttonIcon"/> Reset date chart</Button> : <></>}
           <Row>
-            <Col xl={100}>
-              {estacaoParametros.length > 1 ? (
-                estacaoParametros.map((param, index) => (
-
+            {estacaoParametros.length > 1 ? (
+              estacaoParametros.map((param, index) => (
+                <Col className="coluna">
                   <Chart className="container_dashboard" options={optionsState[index] ? optionsState[index] : {}} />
-                ))
-              ) : (
-                <Chart className="container_dashboard" options={options} />
-              )}
-            </Col>
+                </Col>
+              ))
+            ) : (
+              <Chart className="container_dashboard" options={options} />
+            )}
           </Row>
           <h4 className="parametrosTitulo">Parâmetros</h4>
           <div className="box-par">
