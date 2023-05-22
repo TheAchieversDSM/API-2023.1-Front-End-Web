2411f058d2aa5ecd8142d636dd1d775da614964f
Maroa Clara Alves de Faria <mclaralvs@gmail.com>
2023-05-22 12:24:05 -0300

    🧪 test: Início integração Cypress <=> Sonarqube.

--- api/sonarqube-scanner.js
+++ api/sonarqube-scanner.js
@@ -1,8 +1,11 @@
 const scanner = require("sonarqube-scanner");
+// sqp_c67769cebb2b311676cc646d92bc6385dbe9fd45
+
 scanner(
   {
     serverUrl: "http://localhost:9000",
-    token: "sqp_8702c1b760c1861fd1b68d3691c96fedef13b836",
+    //token: "sqp_154f75766d1957e4e6103215aa3c547cf489087b",
+    login: "admin",
     password: "tecsus",
     options: {
       "sonar.sources": "./src",
