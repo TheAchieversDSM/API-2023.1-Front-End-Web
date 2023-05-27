6c773f9d8763a78406a542dcb7fd0aa4a56389a2
MatheusSakuragui <matheus.lemesakuragui@gmail.com>
2023-05-27 18:18:06 -0300

    corrigido

--- api/build-script.sh
+++ api/build-script.sh
@@ -2,10 +2,10 @@
 
 echo "VERCEL_GIT_COMMIT_REF: $VERCEL_GIT_COMMIT_REF"
 
-if [[ "$VERCEL_GIT_COMMIT_REF" == "pre-release" ]]; then
+if [[ "$VERCEL_GIT_COMMIT_REF" == "pre-release" || "$VERCEL_GIT_COMMIT_REF" == "refs/heads/pre-release" ]]; then
     echo "✅ - Realizando Build para Preview"
     exit 0;
-elif [[ "$VERCEL_GIT_COMMIT_REF" == "main" ]]; then
+elif [[ "$VERCEL_GIT_COMMIT_REF" == "main" || "$VERCEL_GIT_COMMIT_REF" == "refs/heads/main"  ]]; then
     echo "✅ - Realizando Build para Production"
     exit 0;
 else
