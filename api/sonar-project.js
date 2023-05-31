46563581d8af82e643e0feec4a475242d53f224a
Maroa Clara Alves de Faria <mclaralvs@gmail.com>
2023-05-31 10:28:25 -0300

    🐛 fix: Ajuste do Sonarqube.

--- api/sonar-project.js
+++ api/sonar-project.js
@@ -10,7 +10,6 @@ sonarqubeScanner(
             "sonar.inclusions": "./src/**/*.ts,src/**/*.tsx",
             "sonar.test.inclusions": "./src/cypress/**/*.cy.ts",
             "sonar.login": process.env.SONAR_LOGIN,
-            "sonar.password": process.env.SONAR_PASSWORD,
             "sonar.javascript.lcov.reportPaths": "./coverage/lcov.info"
         },
     },
