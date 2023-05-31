6824fbd7f462337291aef3e46a89bbc3fdcd205d
Maroa Clara Alves de Faria <mclaralvs@gmail.com>
2023-05-31 10:41:59 -0300

    🐛 fix: Credenciais.

--- api/sonar-project.js
+++ api/sonar-project.js
@@ -3,12 +3,13 @@ const sonarqubeScanner = require("sonarqube-scanner");
 
 sonarqubeScanner(
     {
-        serverUrl: "http://localhost:9000",
+        serverUrl: "https://sonarcloud.io",
         options: {
             "sonar.sources": "./src",
             "sonar.tests": "./src",
             "sonar.inclusions": "./src/**/*.ts,src/**/*.tsx",
             "sonar.test.inclusions": "./src/cypress/**/*.cy.ts",
+            "sonar.key": "api-2023-1-front-end            ",
             "sonar.login": process.env.SONAR_LOGIN,
             "sonar.javascript.lcov.reportPaths": "./coverage/lcov.info"
         },
