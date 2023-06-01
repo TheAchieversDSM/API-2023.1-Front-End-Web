1b029631642395cba1f11e6a372e8046a0b6d3be
Maroa Clara Alves de Faria <mclaralvs@gmail.com>
2023-06-01 09:27:42 -0300

    🐛 fix: Ajuste do arquivo do Sonar-Scanner.

--- api/sonar-project.js
+++ api/sonar-project.js
@@ -4,15 +4,17 @@ const sonarqubeScanner = require("sonarqube-scanner");
 sonarqubeScanner(
     {
         serverUrl: "https://sonarcloud.io",
+        token: process.env.SONAR_LOGIN,
         options: {
+            "sonar.projectName": "API-2023.1-Front-End-Web",
             "sonar.sources": "./src",
             "sonar.tests": "./src",
             "sonar.inclusions": "./src/**/*.ts,src/**/*.tsx",
             "sonar.test.inclusions": "./src/cypress/**/*.cy.ts",
-            "sonar.key": "api-2023-1-front-end",
+            "sonar.key": "achievers",
             "sonar.login": process.env.SONAR_LOGIN,
             "sonar.javascript.lcov.reportPaths": "./coverage/lcov.info",
-            "sonar.organization": "TheAchieversDSM",
+            "sonar.organization": "achievers",
         },
     },
     () => {
